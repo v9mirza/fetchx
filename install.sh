c96ca8c14e35299e10752a10c3902b731f1f0546
v9mirza <v9mirza@gmail.com>
2025-12-22 15:42:26 +0000

    added more detailed info

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
+# -------------------------------
+# Dependency check
+# -------------------------------
 if ! command -v python3 >/dev/null; then
   echo "Error: python3 is required but not installed."
   echo
@@ -10,8 +14,9 @@ if ! command -v python3 >/dev/null; then
   exit 1
 fi
 
-
-
+# -------------------------------
+# Config
+# -------------------------------
 REPO="v9mirza/fetchx"
 RAW_BASE="https://raw.githubusercontent.com/$REPO/main"
 BIN_NAME="fetchx"
@@ -19,17 +24,26 @@ INSTALL_PATH="/usr/local/bin/$BIN_NAME"
 
 echo "Installing fetchx..."
 
+# -------------------------------
+# Download
+# -------------------------------
 tmp_dir="$(mktemp -d)"
-
 curl -fsSL "$RAW_BASE/$BIN_NAME" -o "$tmp_dir/$BIN_NAME"
 
 if [ ! -s "$tmp_dir/$BIN_NAME" ]; then
   echo "Error: failed to download fetchx"
+  rm -rf "$tmp_dir"
   exit 1
 fi
 
+# -------------------------------
+# Install
+# -------------------------------
 sudo install -m 755 "$tmp_dir/$BIN_NAME" "$INSTALL_PATH"
 
+# -------------------------------
+# Cleanup
+# -------------------------------
 rm -rf "$tmp_dir"
 
 echo "fetchx installed successfully."
